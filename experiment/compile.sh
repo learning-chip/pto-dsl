c608408cfadacb4388236bd97ea88dff0d52c87f
learning-chip <jiawei.zhuang@outlook.com>
2026-02-02 15:33:40 +0000

    bisheng

--- experiment/compile.sh
+++ experiment/compile.sh
@@ -1,2 +1,12 @@
 python ./relu_builder.py > relu.pto
-ptoas relu.pto -o relu.cpp
+ptoas relu.pto -o relu.cpp 2>debug_as.log
+
+# temporary fix for macro guards, better let `ptoas` insert it
+(echo '#if __CCE_AICORE__ == 220 && defined(__DAV_C220_VEC__)'; cat relu.cpp; echo '#endif') > relu.cpp.tmp && mv relu.cpp.tmp relu.cpp
+
+bisheng \
+    -I${PTO_LIB_PATH}/include/pto \
+    -fPIC -shared -O2 -std=c++17 \
+    --npu-arch=dav-2201 -DMEMORY_BASE \
+    ./relu.cpp \
+    -o ./relu_kernel.so
