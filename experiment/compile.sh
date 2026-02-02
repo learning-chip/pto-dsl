e6ed6ff4812c711312884e3febfbe8763a8aba66
learning-chip <jiawei.zhuang@outlook.com>
2026-02-02 15:43:57 +0000

    edit cpp

--- experiment/compile.sh
+++ experiment/compile.sh
@@ -1,12 +1,12 @@
 python ./relu_builder.py > relu.pto
-ptoas relu.pto -o relu.cpp 2>debug_as.log
+ptoas relu.pto -o relu_generated.cpp 2>debug_as.log
 
-# temporary fix for macro guards, better let `ptoas` insert it
-(echo '#if __CCE_AICORE__ == 220 && defined(__DAV_C220_VEC__)'; cat relu.cpp; echo '#endif') > relu.cpp.tmp && mv relu.cpp.tmp relu.cpp
+# manually change: relu_generated.cpp -> relu_edited.cpp 
+# TODO: use a Python script to patch source code
 
 bisheng \
     -I${PTO_LIB_PATH}/include/pto \
     -fPIC -shared -O2 -std=c++17 \
     --npu-arch=dav-2201 -DMEMORY_BASE \
-    ./relu.cpp \
+    ./relu_edited.cpp \
     -o ./relu_kernel.so
