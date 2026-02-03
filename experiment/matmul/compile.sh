067cba63356ca2f36cd2b81e800afc6917e97eb0
learning-chip <jiawei.zhuang@outlook.com>
2026-02-03 15:28:06 +0000

    add python source

--- experiment/matmul/compile.sh
+++ experiment/matmul/compile.sh
@@ -1,3 +1,9 @@
+python matmul.py > matmul.pto
+ptoas matmul.pto -o matmul_generated.cpp
+
+# manually change: matmul_generated.cpp -> matmul_edited.cpp 
+# TODO: use a Python script to patch source code
+
 bisheng -fPIC -shared -xcce -O2 -std=c++17 \
     --npu-arch=dav-2201 -DMEMORY_BASE \
     -I${PTO_LIB_PATH}/include \
