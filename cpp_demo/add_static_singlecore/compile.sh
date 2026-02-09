4c58b25ee5e28aae8bbe992573e4d2108278d4af
learning-chip <jiawei.zhuang@outlook.com>
2026-02-09 21:53:14 +0000

    add full compile flags

--- cpp_demo/add_static_singlecore/compile.sh
+++ cpp_demo/add_static_singlecore/compile.sh
@@ -1,6 +1,15 @@
-bisheng -fPIC -shared -xcce -O2 -std=c++17 \
-    --npu-arch=dav-2201 -DMEMORY_BASE \
+bisheng \
     -I${ASCEND_TOOLKIT_HOME}/include \
+    -fPIC -shared -D_FORTIFY_SOURCE=2 -O2 -std=c++17 \
+    -Wno-macro-redefined -Wno-ignored-attributes -fstack-protector-strong \
+    -xcce -Xhost-start -Xhost-end \
+    -mllvm -cce-aicore-stack-size=0x8000 \
+    -mllvm -cce-aicore-function-stack-size=0x8000 \
+    -mllvm -cce-aicore-record-overflow=true \
+    -mllvm -cce-aicore-addr-transform \
+    -mllvm -cce-aicore-dcci-insert-for-scalar=false \
+    --npu-arch=dav-2201 -DMEMORY_BASE \
+    -std=gnu++17 \
     ./add.cpp \
     -o ./add.so
 
